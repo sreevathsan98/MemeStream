23474192862e1b7e105511a43d04675ddc3cb51e
SREEVATHSAN K <sreevathsan.k@hp.com>
2021-02-11 22:49:31 +0530

    adding cors

--- Xmeme/src/server.js
+++ Xmeme/src/server.js
@@ -51,7 +51,18 @@ function handleError(res, reason, message, code) {
   }
 
 
-  
+  app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if(req.Method==='OPTIONS'){
+      res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+    }
+    next();
+  })
   app.get("/memes",function(req, res) {
     db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
       if (err) {
@@ -62,6 +73,18 @@ function handleError(res, reason, message, code) {
     });
   });
 
+  app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if(req.Method==='OPTIONS'){
+      res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+    }
+    next();
+  })
   app.post("/memes",function(req, res) {
     var newContact = req.body;
     //newContact.createDate = new Date();
